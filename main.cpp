51bac4ff027038fe22c7d516bf7a7b5a67efd540
Asus <neverov.aa@phystech.edu>
2021-04-03 03:22:02 +0300

    Modificate

--- main.cpp
+++ main.cpp
@@ -6,23 +6,50 @@ int main()
 {
 
     std::fstream file_out;
-    std::string filename="MyFile.txt";
+//    char *filename=new char[20];
+std::string filename="MyFile";
     double ar_cur[50]{0};
     double ar_nx[50]{0};
     double ar_fin[50]{0};
     int k=0;
-
+    double _if_plus_[50]{0};
+    double _if_minus_[50]{0};
+    double kappa_1=0,kappa_2=0;
     for (int i=0;i<50;i++)
     {
 
         ar_cur[i]=std::sqrt(std::exp(-(i-2)*(i-2)));
 
     }
+    for  (int v=-5;v<0;v++)
+    {
+
+        kappa_1+=v*std::sqrt(std::exp(-v*v));
+
+    }
+    for  (int v=1;v<6;v++)
+    {
+
+        kappa_2+=v*std::sqrt(std::exp(-v*v));
+
+    }
+    for (int i=0;i<6;i++)
+    {
+
+        _if_minus_[i]=std::sqrt(std::exp(-i*i));
+
+    }
+    for (int i=0;i<6;i++)
+    {
+
+        _if_plus_[i]=-std::sqrt(std::exp(-i*i))*kappa_1/kappa_2;
+
+    }
     ar_cur[49]=0;
     for (int t=1;t<100;t++)
     {
 
-file_out.open(filename, std::fstream::in | std::fstream::out | std::fstream::app);
+        file_out.open(filename, std::fstream::in | std::fstream::out | std::fstream::app);
 //        sprintf(filename,"out_%03d.txt",t);
         for (int v=-5;v<6;v++)
         {
@@ -38,7 +65,7 @@ file_out.open(filename, std::fstream::in | std::fstream::out | std::fstream::app
                     ar_fin[j]+=ar_nx[j];
 
                 }
-                ar_fin[0]=ar_nx[0]=0;
+                ar_fin[0]=ar_nx[0]=_if_plus_[v-1];
 
             }
             if (v<0)
@@ -51,7 +78,7 @@ file_out.open(filename, std::fstream::in | std::fstream::out | std::fstream::app
                     ar_fin[j]+=ar_nx[j];
 
                 }
-                ar_fin[49]=ar_nx[49]=0;
+                ar_fin[49]=ar_nx[49]=_if_minus_[std::abs(v)-1];
 
             }
             if (v==0)
@@ -70,13 +97,14 @@ file_out.open(filename, std::fstream::in | std::fstream::out | std::fstream::app
         }
         for (int i=0;i<50;i++)
         {
+            if (ar_fin[i]!=1 && ar_fin[i]>0) {
                 file_out << k << '\t' << std::abs(ar_fin[i]) << std::endl;
                 ar_fin[i] = 0;
+            }
             k++;
 
         }
         file_out<<" "<<std::endl;
-        k=0;
         for (int i=0;i<50;i++)
         {
 
