2475f8fb0658ee0b4f2189df170e2cf02c77b34d
Yehor Komarov <geoolekom@gmail.com>
2021-04-13 20:00:53 +0300

    Переписали, разделив на логические блоки

--- main.cpp
+++ main.cpp
@@ -2,84 +2,63 @@
 #include <cstdio>
 #include <fstream>
 #include <iostream>
+const int N = 50, V = 11, T = 100;
+const double dv = 1.0, dx = 10.0, dt = 1.0;
+
+double get_V(int id) { return dv * (id - V / 2) - dv / 2; }
+
+void set_initial_data(double other[N][V]) {
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < V; j++) {
+            auto v = get_V(j);
+            auto x = (i - N / 2);
+            other[i][j] = std::exp(-v * v / 2) * std::exp( - x*x / 2);
+        }
+    }
+}
 
 int main() {
 
-  std::fstream file_out;
-  //    char *filename=new char[20];
-  char filename[100];
-  double ar_cur[50]{0};
-  double ar_nx[50]{0};
-  double ar_fin[50]{0};
-  int k = 0;
-  double _if_plus_[50]{0};
-  double _if_minus_[50]{0};
-  double kappa_1 = 0, kappa_2 = 0;
-  for (int i = 0; i < 50; i++) {
-    ar_cur[i] = std::sqrt(std::exp(-(i - 2) * (i - 2)));
-  }
-  for (int v = -5; v < 0; v++) {
-    kappa_1 += v * std::sqrt(std::exp(-v * v));
-  }
-  for (int v = 1; v < 6; v++) {
-    kappa_2 += v * std::sqrt(std::exp(-v * v));
-  }
-  for (int i = 0; i < 6; i++) {
-    _if_minus_[i] = std::sqrt(std::exp(-i * i));
-  }
-  for (int i = 0; i < 6; i++) {
+    char filename[100];
+    double ar_current[N][V]{0};
+    double ar_next[N][V]{0};
+    double ar_concentration[N]{0};
+    set_initial_data(ar_current);
+    std::fstream file_out;
+    for (int k = 0; k < T; k++) {
+        for (int i = 0; i < N; i++) {
 
-    _if_plus_[i] = -std::sqrt(std::exp(-i * i)) * kappa_1 / kappa_2;
-  }
-  ar_cur[49] = 0;
-  for (int t = 1; t < 100; t++) {
+            for (int j = 0; j < V; j++) {
+                auto v = get_V(j);
+                double gamma = v * dt / dx;
 
-    sprintf(filename, "data/out_%03d.txt", t);
-    file_out.open(filename, std::fstream::out);
-    for (int v = -5; v < 6; v++) {
-
-      double G = std::abs(v * 0.1);
-      if (v > 0) {
-
-        for (int j = 1; j < 50; j++) {
-
-          ar_nx[j] = ar_cur[j] - G * (ar_cur[j] - ar_cur[j - 1]);
-          ar_fin[j] += ar_nx[j];
+                if (v > 0) {
+                    if (i == 0) {
+                        ar_next[0][j] = 0;
+                    } else {
+                        ar_next[i][j] = ar_current[i][j] - gamma * (ar_current[i][j] - ar_current[i - 1][j]);
+                    }
+                } else {
+                    if (i == N - 1) {
+                        ar_next[N - 1][j] = 0;
+                    } else {
+                        ar_next[i][j] = ar_current[i][j] - gamma * (ar_current[i + 1][j] - ar_current[i][j]);
+                    }
+                }
+            }
         }
-        ar_fin[0] = ar_nx[0] = _if_plus_[v - 1];
-      }
-      if (v < 0) {
+        std::swap(ar_current, ar_next);
 
-        for (int j = 1; j < 49; j++) {
-
-          ar_nx[j] = ar_cur[j + 1] - G * (ar_cur[j + 1] - ar_cur[j]);
-          ar_fin[j] += ar_nx[j];
+        sprintf(filename, "data/out_%03d.txt", k);
+        file_out.open(filename, std::fstream::out);
+        for (int i = 0; i < N; i++) {
+            ar_concentration[i] = 0;
+            for (int j = 0; j < V; j++) {
+                ar_concentration[i] += ar_current[i][j];
+            }
+            file_out << i << '\t' << ar_concentration[i] << std::endl;
         }
-        ar_fin[49] = ar_nx[49] = _if_minus_[std::abs(v) - 1];
-      }
-      if (v == 0) {
-
-        for (int j = 0; j < 50; j++) {
-
-          ar_nx[j] = ar_cur[j];
-          ar_fin[j] += ar_nx[j];
-        }
-      }
+        file_out.close();
     }
-    k = 0;
-    for (int i = 0; i < 50; i++) {
-      if (ar_fin[i] != 1 && ar_fin[i] > 0) {
-        file_out << k << '\t' << std::abs(ar_fin[i]) << std::endl;
-        ar_fin[i] = 0;
-      }
-      k++;
-    }
-    file_out << " " << std::endl;
-    for (int i = 0; i < 50; i++) {
-
-      ar_cur[i] = ar_nx[i];
-    }
-    file_out.close();
-  }
-  return 0;
 }
